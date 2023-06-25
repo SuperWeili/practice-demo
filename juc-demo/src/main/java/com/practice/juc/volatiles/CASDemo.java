40c1c591b0833bb45717059f6a6432da96300864
SuperWeili <260497380@qq.com>
2023-06-25 19:24:25 +0800

    feat: CAS 比较并交换

---
+++ juc-demo/src/main/java/com/practice/juc/volatiles/CASDemo.java
@@ -0,0 +1,18 @@
+package com.practice.juc.volatiles;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author LW
+ * @version 1.0
+ * @date 2021/12/2 22:06
+ */
+public class CASDemo {
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger(5);
+        System.out.println(atomicInteger.compareAndSet(5, 2021)+"" +
+                "\t current value:"+ atomicInteger.get());
+        System.out.println(atomicInteger.compareAndSet(5, 1998)+"" +
+                "\t current value:"+ atomicInteger.get());
+    }
+}
