ef6f68c72a66b695a9fb30ee65a557deb0de3229
SuperWeili <260497380@qq.com>
2023-06-27 22:59:33 +0800

    feat: spring- 延迟初始化加载，主要区别就是在上下文生命周期之前后来进行输出

--- spring-bean-demo/src/main/java/com/practice/spring/bean/factory/BeanInitializationDemo.java
+++ spring-bean-demo/src/main/java/com/practice/spring/bean/factory/BeanInitializationDemo.java
@@ -2,6 +2,7 @@ package com.practice.spring.bean.factory;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
 /**
  * bean 初始化Demo {@link AnnotationConfigApplicationContext}
@@ -19,13 +20,17 @@ public class BeanInitializationDemo {
 		applicationContext.register(BeanInitializationDemo.class);
 		// 启动Spring应用上下文
 		applicationContext.refresh();
+		// 非延迟初始化在Spring应用上下文启动完成后，被初始化
+		System.out.println("Spring 应用上下文已被启动。。");
 		// 依赖查找UserFactory
 		UserFactory bean = applicationContext.getBean(UserFactory.class);
+		System.out.println(bean);
 		// 关闭Spring上下文
 		applicationContext.close();
 	}
 
 	@Bean(initMethod = "initUserFactory")
+	@Lazy(value = false)
 	public UserFactory userFactory(){
 		return new DefaultUserFactory();
 	}
